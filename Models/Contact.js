1ce415dcd43d74303d62597d58f25317c759b706
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-12 17:00:47 +0100

    update contact schema add timestamps

--- Models/Contact.js
+++ Models/Contact.js
@@ -39,7 +39,7 @@ let ContactSchema = mongoose.Schema({
             }
         ]
     }
-});
+},{ timestamps: { createdAt: true, updatedAt: true } });
 
 let Contact = mongoose.model("Contact", ContactSchema);
 
