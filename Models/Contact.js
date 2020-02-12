1d04953b71bd200d992d55326711b414db998baf
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-12 16:33:20 +0100

    feat(Models) : update contact model add unique attr

--- Models/Contact.js
+++ Models/Contact.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 
 let ContactSchema = mongoose.Schema({
-    // _id: mongoose.Schema.Types.ObjectId, 
-    name: { 
+    // _id: mongoose.Schema.Types.ObjectId,
+    contactName: {
+        type: String,
+        required: [
+            true, "contactName is required"
+        ],
+        unique: [
+            true, "Duplicated contact name"
+        ]
+    },
+    fullName: { 
         type: String, 
         required: [
             true, "Name is required"
         ] 
     },
     number: { 
-        type: Number, 
+        type: String, 
         required: [
-            true, "Number is required"
-        ] 
+            true, "Phone Number is required"
+        ],
+        unique: [
+            true, "Duplicated Phone Number"
+        ]
     },
     gender: { 
         type: String,
